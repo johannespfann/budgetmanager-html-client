15b4b5084e817927f2e6c802ba52feab2ffca7fe
Johannes Pfann <jopf88@googlemail.com>
2017-05-15 23:15:29 +0200

    Implement blank add-entry.component.html and ts

---
+++ src/app/addentrycomponent/add-entry.component.ts
@@ -0,0 +1,38 @@
+
+
+import { Category } from "../models/category";
+import { Tag } from "../models/tag";
+import { Entry } from "../models/entry";
+
+export class AddEntryComponent {
+
+    sum: number;
+
+    memo: string;
+
+    current_date: string;
+
+    category: Category;
+
+    tags: Array<Tag>;
+
+    constructor(){
+        console.log('Invoke AddEntryComponent');
+    }
+
+
+    save(){
+        let entry:Entry;
+
+        entry.sum = this.sum;
+        entry.memo = this.memo;
+        entry.current_date = this.current_date;
+        entry.tags = this.tags;
+
+        // TODO save entry
+
+        console.log('save : ' + entry);
+
+    }
+
+}
