3492d6a03a97a19391c0863f3359c22e98b016bd
Johannes Pfann <jopf88@googlemail.com>
2017-05-22 21:36:29 +0200

    Added loginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes }  from '@angular/router';
 import { WelcomeComponent } from "./welcomecomponent/welcome.component";
 import { AddEntryComponent } from "./addentrycomponent/add-entry.component";
 import { PageNotFoundComponent } from "./errorcomponents/page-not-found.component";
+import { LoginComponent } from "./logincomponent/login.component";
 
 const appRoutes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'addentry-component',        component: AddEntryComponent },
+  { path: 'welcome',            component: WelcomeComponent },
+  { path: 'newentry', component: AddEntryComponent },
+  { path: 'login', component: LoginComponent },
   { path: '',   redirectTo: '/welcome', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AddEntryComponent } from "./addentrycomponent/add-entry.component";
 import { WelcomeComponent } from "./welcomecomponent/welcome.component";
 import { PageNotFoundComponent } from "./errorcomponents/page-not-found.component";
+import { LoginComponent } from "./logincomponent/login.component";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
@@ -14,7 +15,8 @@ import { PageNotFoundComponent } from "./errorcomponents/page-not-found.componen
     AppComponent,
     WelcomeComponent,
     AddEntryComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
     ],
   bootstrap:    [ AppComponent ]
 })
