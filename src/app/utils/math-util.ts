0a26698fcd97796c1fe5aefd8efb365470d1acc4
Johannes Pfann <jopf88@googlemail.com>
2017-06-03 22:18:48 +0200

    Implemented MathUtil

---
+++ src/app/utils/math-util.ts
@@ -0,0 +1,30 @@
+export class MathUtil{
+    
+    /**
+     * Convert a number to a postitiv one. If the number 
+     * is 0 or a positiv number,
+     * the number will returned.
+     * @param value 
+     */
+    public static convertToPositiv(value: number){
+        if(value > 0 || value == 0){
+            return value;
+        }
+        return value * -1;
+    }
+    
+    /**
+     * Convert a number to a negativ one. If the number 
+     * is 0 or a negativ number,
+     * the number will returned.
+     * @param value 
+     */
+    public static convertToNegativ(value: number){
+        if(value < 0 || value == 0){
+            value = value
+        }
+        return value * -1;
+    }
+
+}
+
