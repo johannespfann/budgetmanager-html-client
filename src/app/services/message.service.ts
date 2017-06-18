fe31c711220820ea85ebc543d9de74911d758834
Johannes Pfann <jopf88@googlemail.com>
2017-06-18 23:20:04 +0200

    new MessageBus

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs/Rx";
+import { Message } from "./message";
+
+
+
+@Injectable()
+export class MessagingService {
+    private message$: Subject<Message>
+
+    constructor(){
+        this.message$ = new Subject<Message>();
+    }
+
+    public publish<T>(message: T): void {
+        const channel = (<any>message.constructor).name;
+        this.message$.next({ channel: channel, data: message });
+    }
+
+    public of<T>(messageType: { new(...args: any[]): T }): Observable<T> {
+        const channel = (<any>messageType).name;
+        return this.message$.filter(m => m.channel === channel).map(m => m.data);
+    }
+}
