9ff699c4cd40bc3f4fce6515be9b011e5e87b0e5
Johannes Pfann <jopf88@googlemail.com>
2017-07-07 23:04:35 +0200

    add logutil

---
+++ src/app/utils/log-util.ts
@@ -0,0 +1,15 @@
+export class LogUtil{
+
+    public static info(aMessage:string) {
+        console.log(aMessage);
+    }
+
+    public static debug(aMessage: string) {
+        console.log(aMessage);
+    }
+
+    public static error(aMessage: string) {
+        console.log(aMessage);
+    }
+
+}
