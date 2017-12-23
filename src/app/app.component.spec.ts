68185474666762bc37128d8bf73ffcbe5ee5d092
Johannes Pfann <jopf88@googlemail.com>
2017-12-23 20:23:32 +0100

    deleted old tests for components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,29 +5,18 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 describe('AppComponent', function () {
-  let de: DebugElement;
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+
   });
 
-  it('should create component', () => expect(comp).toBeDefined() );
+  it('should create component', () => {
+    
+  });
 
   it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.textContent).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+
   });
+  
 });
