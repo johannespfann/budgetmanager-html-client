590fc8204278ea53625ee1cc615c4c285d1aac44
Johannes Pfann <jopf88@googlemail.com>
2018-01-06 15:18:27 +0100

    added constructor to login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
+import { LogUtil } from "../utils/log-util";
 
 
 @Injectable()
 export class LoginService {
     
     constructor() {
-        
+        LogUtil.info(this,'Init LoginService');
     }
 
-    
+       
 }
