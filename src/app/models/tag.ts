f497f4b09bc5452955c46e9a645ac85f8b7c8c5a
Johannes Pfann <jopf88@googlemail.com>
2018-02-19 21:49:49 +0100

    implemented tag

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -2,8 +2,19 @@
 
 export class Tag{
 
-    id: string;
+    public id: string;
 
-    name: string;
+    public name: string;
     
+    constructor(){
+        // default
+    }
+
+    public copy(aTag: Tag): Tag {
+        let tag: Tag = new Tag();
+        tag.name = aTag.name;
+        tag.id = aTag.id;
+        return tag;
+    }
+
 }
