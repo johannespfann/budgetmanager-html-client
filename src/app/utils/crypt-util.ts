6228ab5e0735d3ea1cb085a16e435aaa067b027b
Johannes <jopf88@googlemail.com>
2018-04-06 07:05:28 +0200

    added blank crypt util with methods

---
+++ src/app/utils/crypt-util.ts
@@ -0,0 +1,20 @@
+
+export class CryptUtil{
+
+    public static encryptString(aMessage: string): string {
+        return '';
+    }
+
+    public static decryptString(aMessage: string): string {
+        return '';
+    }
+
+    public static encryptNumber(aMessage: number): number {
+        return 0;
+    }
+
+    public static decryptNumber(aMessage: number): number {
+        return 0;
+    }
+
+}
