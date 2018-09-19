68412b04466036cd2077794b4f8f0dcfef08af1f
Johannes <jopf88@googlemail.com>
2018-09-19 21:44:37 +0200

    added facades for services

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,29 @@
+
+export class LoginService {
+
+    constructor() {
+
+    }
+
+    // TODO
+    public registerUser(aBaseUrl: string, aUser: User, aPassword: String): Observable<any> {
+        return null;
+    }
+    // TODO
+    public activateUser(aBaseUrl: string, aName: String, aCode: String): Observable<any> {
+        return null;
+    }
+    // TODO
+    public resendActivationEmail(aBaseUrl: string, aName: string, aEmail: string): Observable<any> {
+        return null;
+    }
+    // TODO
+    public login(aBaseUrl: string, aEmail: String, aPassword: String): Observable<any> {
+        return null;
+    }
+    // TODO
+    public logout(aBaseUrl: string, aName: String, accessToken: String): void {
+        return null;
+    }
+
+}
