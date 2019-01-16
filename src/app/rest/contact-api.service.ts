fcffd88805a97993c2e047bb0fbb95ec20ab30f2
johannes <jopf88@googlemail.com>
2019-01-16 20:03:54 +0100

    beautified

--- src/app/rest/contact-api.service.ts
+++ src/app/rest/contact-api.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { ContactMessage } from "../models/contact-message";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactMessage } from '../models/contact-message';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ContactApiService {
