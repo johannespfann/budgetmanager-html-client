93cc61bec1761aa50c7f1089375efd6ed9a35aca
johannes <jopf88@googlemail.com>
2019-01-31 22:58:39 +0100

    implemented new method with new short hash

--- src/app/utils/hash-util.ts
+++ src/app/utils/hash-util.ts
@@ -5,11 +5,23 @@ import { LogUtil } from './log-util';
 export class HashUtil {
 
     /**
-     * Generate a unique hash of an given input
+     * Generate a unique hash of an random number
      * @param aInput
      */
     public static getUniqueHash(): string {
         return CryptoJS.SHA1(MathUtil.generateRandom().toString());
+    }
+
+    /**
+     * Generate a unique hash of an random number and split the result to a string of max. lenght 6
+     * @param aInput
+     */
+    public static getShortUniqueHash(): string {
+        const hash = this.getUniqueHash().toString();
+        if (hash.length >= 6) {
+            return hash.substring(0, 6);
+        }
+        return hash;
     }
 
 }
