ffcdc1272fb8fac87f2091a8132d0548e628644d
johannes <jopf88@googlemail.com>
2019-02-12 21:36:52 +0100

    added new constclass

---
+++ src/app/standingordermanagement/standing-order-const.ts
@@ -0,0 +1,12 @@
+
+
+export class StandingOrderConst {
+
+
+    public static readonly MONTHLY_PATTERN = 'monthly';
+
+    public static readonly QUARTER_PATTERN = 'quarter';
+
+    public static readonly YEARLY_PATTERN = 'yearly';
+
+}
